26b247ab58764bd6515a934bfb5dc98b6abc50a4
vijayzennaxx <vijayzennaxx@gmail.com>
2022-04-06 10:51:23 +0530

    add template insert

--- public/app-assets/js/template.js
+++ public/app-assets/js/template.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     //console.log(add_form);
     if($('#form_template').length > 0)
@@ -47,7 +48,9 @@ $(document).ready(function(){
         })
     }
     
-
+    $('#btn_create_new_template').on('click',function(){
+        window.location.href="/templates/add";
+    })
     // add templates
     if($('#basic-datatable').length > 0)
     {
@@ -74,8 +77,17 @@ $(document).ready(function(){
                          $button_group+='<a href="edit/'+data._id+'" class="btn btn-secondary">Edit</a>';
                         
                         
-                        $button_group+='<button type="button" class="btn btn-secondary">View</button><button type="button" class="btn btn-secondary btn-status" data-value="'+data.isActive+'" data-id="'+data._id+'">Active</button><button type="button"  class="btn btn-del-temp btn-success" data-id="'+data._id+'">Delete</button></div>';
-                        
+                        $button_group+='<button type="button" class="btn btn-secondary">View</button>';
+                        if(data.isActive=="Active")
+                        {
+                            $button_group+='<button type="button" class="btn btn-success btn-status" data-value="'+data.isActive+'" data-id="'+data._id+'">Active</button>';
+                        }
+                        else
+                        {
+                            $button_group+='<button type="button" class="btn btn-secondary btn-status" data-value="'+data.isActive+'" data-id="'+data._id+'">InActive</button>';
+
+                        }
+                        $button_group+='<button type="button" class="btn btn-del-temp btn-danger" data-id="'+data._id+'">Delete</button></div>';
                         table.row.add( [
                          counter++,
                          data.TemplateCode,
@@ -96,72 +108,122 @@ $(document).ready(function(){
             });  
         } 
 
-        $('body').on('click','.btn-del-temp',function(){
-            var id = $(this).data('id');
-            var $this = $(this);
-            $.ajax({
-                url:'/templates/delete/'+id,  
-                method:'delete',  
-                dataType:'json',
-                success:function(response){
-                    if(response.Status==1)
-                    {
-                        sweetAlert("success", response.msg, "success");
-                        table
-                        .row( $this.parents('tr') )
-                        .remove()
-                        .draw();
-                    }
-                    else
-                    {
-                        sweetAlert("fail", response.msg, "fail");
-                    }
-                },
-                error:function(response){
-                    alert('server error');
-                }
-            })
-
-        })
-        $('body').on('click','.btn-status',function(){
-            var id = $(this).data('id');
-            var isActive = $(this).data('value');
-            var $this = $(this);
-            var btn_class = '';
-            var btn_html = '';
-            if(isActive=="Active")
-            {
-                isActive="InActive";
-                btn_class = 'btn-secondary';
-                
-            }
-            else
-            {
-                isActive="Active";
-                btn_class = 'btn-success';
-            }
-            $.ajax({
-                url:'/templates/update/'+id,  
-                method:'put',  
-                dataType:'json',
-                data:{isActive:isActive},
-                success:function(response){
-                    $this.data('value',isActive);
-                    $this.removeClass('btn-secondary');
-                    $this.removeClass('btn-success');
-                    $this.addClass(btn_class);
-                    $this.html(isActive);
-                    sweetAlert("success", response.msg, "success");
-                  //$('#project_id').val(response.id);
-                  //console.log(response);
-                  //numberedStepper.next();
-                },
-                error:function(textStatus,error)
-                {
-                  return false;
-                }
-              });
-
-        })
     }
+    $('body').on('click','.btn-del-temp',function(){
+        
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            
+            if (willDelete) {
+                var id = $(this).data('id');
+                var $this = $(this);
+                $.ajax({
+                    url:'/templates/delete/'+id,  
+                    method:'delete',  
+                    dataType:'json',
+                    success:function(response){
+                        if(response.Status==1)
+                        {
+                            
+                            table
+                            .row( $this.parents('tr') )
+                            .remove()
+                            .draw();
+                        }
+                        else
+                        {
+                            
+                        }
+                    },
+                    error:function(response){
+                        alert('server error');
+                    }
+                })
+              swal("Template has been deleted!", {
+                icon: "success",
+              });
+            } else {
+              swal("Your imaginary file is safe!");
+            }
+          });
+        // if(confirmmsg)
+        // {
+
+        //     var id = $(this).data('id');
+        //     var $this = $(this);
+        //     $.ajax({
+        //         url:'/templates/delete/'+id,  
+        //         method:'delete',  
+        //         dataType:'json',
+        //         success:function(response){
+        //             if(response.Status==1)
+        //             {
+        //                 sweetAlert("success", response.msg, "success");
+        //                 table
+        //                 .row( $this.parents('tr') )
+        //                 .remove()
+        //                 .draw();
+        //             }
+        //             else
+        //             {
+        //                 sweetAlert("fail", response.msg, "fail");
+        //             }
+        //         },
+        //         error:function(response){
+        //             alert('server error');
+        //         }
+        //     })
+        // }
+        // else
+        // {
+        //     return false;
+        // }
+
+    })
+    $('body').on('click','.btn-status',function(){
+        var id = $(this).data('id');
+        var isActive = $(this).data('value');
+        var $this = $(this);
+        var btn_class = '';
+        var btn_html = '';
+        if(isActive=="Active")
+        {
+            isActive="InActive";
+            btn_class = 'btn-secondary';
+            
+        }
+        else
+        {
+            isActive="Active";
+            btn_class = 'btn-success';
+        }
+        $.ajax({
+            url:'/templates/update/'+id,  
+            method:'put',  
+            dataType:'json',
+            data:{isActive:isActive},
+            success:function(response){
+                $this.data('value',isActive);
+                $this.removeClass('btn-secondary');
+                $this.removeClass('btn-success');
+                $this.addClass(btn_class);
+                $this.html(isActive);
+                sweetAlert("success", response.msg, "success");
+              //$('#project_id').val(response.id);
+              //console.log(response);
+              //numberedStepper.next();
+            },
+            error:function(textStatus,error)
+            {
+              return false;
+            }
+          });
+
+    })
 })
